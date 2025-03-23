5f954923081f75c4f53d1ff7ffd43c292cd7f3b6
Bozhidar Stoyanov <BHStoyanov23@codingburgas.bg>
2025-03-23 22:08:06 +0200

    Implement exam generation logic

--- Project_Team_Yager/Project_Team_Yager/question.cpp
+++ Project_Team_Yager/Project_Team_Yager/question.cpp
@@ -20,3 +20,19 @@ vector<question> selectRandomSubset(vector<question> questionsList, size_t count
     vector<question> subset(shuffledQuestions.begin(), shuffledQuestions.begin() + count);
     return subset;
 }
+
+vector<question> generate_exam() {
+    vector<question> exam;
+    random_device rd;
+    mt19937 rng(rd());
+    for (int category = 0; category < CATEGORY_COUNT; ++category) {
+        vector<question> categoryQuestions = filterQuestionsByCategory(questions, category);
+        if (categoryQuestions.size() < 4) {
+            throw runtime_error("Not enough questions in category: " + category_names[category]);
+        }
+        vector<question> selectedQuestions = selectRandomSubset(categoryQuestions, 4, rng);
+        exam.insert(exam.end(), selectedQuestions.begin(), selectedQuestions.end());
+    }
+    shuffle(exam.begin(), exam.end(), rng);
+    return exam;
+}
